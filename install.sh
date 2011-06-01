24eca01efd1724543cffe433c8a91e1b4d0b935e
trondham <trondham@e53b7cee-c147-0410-b3a0-ae4c1fa63963>
2011-06-01 15:04:00 +0000

    foo
    git-svn-id: svn+ssh://vcs-usit.uio.no/svnroot/usit-unix-intern/trunk/usit-nagios-plugins/trondham/check_openmanage@20325 e53b7cee-c147-0410-b3a0-ae4c1fa63963

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 
 # Default install locations
 def_plugindir=${libdir}/nagios/plugins
-def_mandir=/usr/share/man/man8
+def_mandir=/usr/share/man
 
 # Find install locations
 if [ "$1" = "-q" ]; then
@@ -27,6 +27,9 @@ else
 	mandir=$def_mandir
     fi
 fi
+
+man5dir=$mandir/man5
+man8dir=$mandir/man8
 
 # Error if plugin dir doesn't exist
 if [ -d $plugindir ]; then
@@ -48,7 +51,8 @@ fi
 
 # Install
 install -p -m 0755 check_openmanage $plugindir
-install -m 0644 check_openmanage.8 $mandir
+install -m 0644 check_openmanage.8 $man8dir
+install -m 0644 check_openmanage.conf.5 $man5dir
 
 # Done
 echo "done."
